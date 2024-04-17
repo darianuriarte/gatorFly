9295d9e4cc6b3ba6ffdb3adc0918da104cacf23a
lizdaniel <80736207+ld2345@users.noreply.github.com>
2024-04-17 16:21:04 -0400

    beginning of styling register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,6 +2,7 @@ import {useState } from 'react'
 import axios from 'axios'
 import {toast} from 'react-hot-toast'
 import { useNavigate } from 'react-router-dom'
+import './register.css'
 
 export default function Register() {
     const navigate = useNavigate()
@@ -35,12 +36,18 @@ const registerUser =  async (e) => {
   return (
     <div>
         <form onSubmit = {registerUser}>
-            <label>Name</label>
-            <input type='text' placeholder ='Please enter your name' value ={data.name} onChange={(e) => setData({...data, name: e.target.value})} />
-            <label>Email</label>
-            <input type='email' placeholder ='Please enter your email'value ={data.email} onChange={(e) => setData({...data, email: e.target.value})} />
-            <label>Password</label>
-            <input type='password' placeholder ='Please enter a password'value ={data.password} onChange={(e) => setData({...data, password: e.target.value})} />
+            <div className="custom-input-container">
+                <label className="custom-label">Name</label>
+                <input className="custom-input" type='text' placeholder ='Please enter your name' value ={data.name} onChange={(e) => setData({...data, name: e.target.value})} />
+            </div><br></br>
+            <div className="custom-input-container">
+                <label className="custom-label">Email</label>
+                <input className="custom-input" type='email' placeholder ='Please enter your email'value ={data.email} onChange={(e) => setData({...data, email: e.target.value})} />
+            </div><br></br>
+            <div className="custom-input-container">
+                <label className="custom-label">Password</label>
+                <input className="custom-input" type='password' placeholder ='Please enter a password'value ={data.password} onChange={(e) => setData({...data, password: e.target.value})} />
+            </div><br></br>
             <button type= 'submit'>Submit</button>
         </form>
     </div>
