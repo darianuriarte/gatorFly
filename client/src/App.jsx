f946ec34bd19ec9bd4d91717e68d3e0456648ac6
darianuriarte <darianuriarte99@gmail.com>
2024-04-30 19:34:54 -0400

    Bug Fix Attempt

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,17 @@
 import './App.css';
 import { Routes, Route, useLocation } from 'react-router-dom';
-import Navbar from './components/NavBar';
+
 import Home from './pages/Home';
 import Calendar from './pages/Calendar';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Flights from './pages/Flights';
-import PrivateRoutes from './components/PrivateRoutes';
+import PrivateRoutes from './componengts/PrivateRoutes';
 
 import axios from 'axios';
 import { Toaster } from 'react-hot-toast';
 import { UserContextProvider } from '../context/userContext';
+import NavBar from './components/NavBar.jsx';
 
 axios.defaults.baseURL = 'https://gatorfly.onrender.com';
 axios.defaults.withCredentials = true;
@@ -24,7 +25,7 @@ function App() {
   return (
     <UserContextProvider>
       <Toaster position='bottom-right' toastOptions={{ duration: 2000 }} />
-      {!isHomePage && !isLogin && !isRegister && <Navbar />}
+      {!isHomePage && !isLogin && !isRegister && <NavBar />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Link, useNavigate} from 'react-router-dom';
 import axios from 'axios';
 
-export default function Navbar() {
+export default function NavBar() {
 
 
 
