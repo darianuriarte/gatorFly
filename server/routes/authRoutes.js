0516a2ea6fd9c70949fe2c118307459db175ba6e
Darian Uriarte <101579669+darianuriarte@users.noreply.github.com>
2024-04-30 20:16:56 -0400

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -7,7 +7,7 @@ const {test, registerUser, loginUser, getProfile, logoutUser} = require('../cont
 router.use(
     cors({
         credentials: true,
-        origin: 'http://localhost:5173'
+        origin: 'https://gatorfly-frontend.onrender.com'
     })
 )
 
