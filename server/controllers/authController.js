d5d70a00b37a70f4f5d89ec79c599f95c79cc073
Darian Uriarte <101579669+darianuriarte@users.noreply.github.com>
2024-05-01 15:43:40 -0400

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -79,19 +79,18 @@ const loginUser = async (req, res) => {
   }
 };
 
-const getProfile =(req, res) =>{
-    const {token} = req.cookies
-    if (token){
-        jwt.verify(token, process.env.JWT_SECRET, {}, (err, user) =>{
-            if (err) throw err;
-            res.json(user)
-        } )
-    } else {
-        res.json(null)
-    }
+const getProfile = (req, res) => {
+  const token = req.headers.authorization;
 
-   
-}
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, {}, (err, user) => {
+      if (err) throw err;
+      res.json(user);
+    });
+  } else {
+    res.json(null);
+  }
+};
 
 const logoutUser = (req, res) => {
     res.clearCookie('token'); // Clear the JWT token stored in the cookie
